0c7c53bbd070dd292992a3336d94e9862ee48725
xg4 <xingor4@gmail.com>
2020-02-15 21:39:53 +0800

    chore: init

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+export default {
+  input: 'src/index.ts',
+  external: ['node-fetch'],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    resolve({ extensions }),
+    commonjs(),
+    babel({
+      extensions,
+      include: ['src/**/*']
+    }),
+    terser()
+  ]
+}
